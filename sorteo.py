77c8d08a2f9f096d8a726b72ad82574a3dc0447a
fjortegan <fjortegan@gmail.com>
2023-04-17 10:03:17 +0200

    first commit

---
+++ sorteo.py
@@ -0,0 +1,8 @@
+import random
+
+alumnado = ["Erik", "Pablo", "Ese", "JuanJoW2",
+        "Luis", "Daniel", "Delegao", "Adrian",
+        "Manuel", "Rafael", "Angel", "Javier", "Dios",
+        "Debora"]
+
+print(random.choice(alumnado))
